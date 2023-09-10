addd48661abef48a98278f31fb2efdea6cf0fbb6
rustam <rustam.raimanovv@gmail.com>
2023-09-10 12:58:51 +0500

    update tests

--- horoscope/tests.py
+++ horoscope/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
+from .views import zodiac_dict
+
 
 # Create your tests here.
 
@@ -14,6 +17,16 @@ class TestHoroscope(TestCase):
         self.assertIn('солнце (с 23 июля по 21 августа)', response.content.decode())
 
     def test_leo_redirect(self):
-        response = self.client.get('/horoscope/5/')
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/horoscope/leo/')
+        for number, zodiac in enumerate(zodiac_dict):
+            response = self.client.get(f'/horoscope/{number + 1}/')
+            self.assertEqual(response.status_code, 302)
+            reverse_path = reverse('horoscope-name', args=(zodiac,))
+            self.assertEqual(response.url, reverse_path)
+
+    def test_all_horoscope_content(self):
+        zodiacs = zodiac_dict
+        for zodiac, description in zodiacs.items():
+            reverse_path = reverse('horoscope-name', args=(zodiac,))
+            response = self.client.get(reverse_path)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(description, response.content.decode())
